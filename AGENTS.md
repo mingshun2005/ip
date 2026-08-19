89574c099ffb6acd2eb2f2202e0ef43132996adf
mingshun529-coder <mingshun529@gmail.com>
2026-08-19 11:33:42 +0800

    Rename chatbot to Duck and print startup and exit messages

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: been programing java for cs2030s
+* IDE and level of expertise: medium
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Duck.java
@@ -0,0 +1,17 @@
+public class Duck {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = " ____             _    \n"
+                + "|  _ \\ _   _  ___| | __\n"
+                + "| | | | | | |/ __| |/ /\n"
+                + "| |_| | |_| | (__|   < \n"
+                + "|____/ \\__,_|\\___|_|\\_\\\n";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Duck.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
