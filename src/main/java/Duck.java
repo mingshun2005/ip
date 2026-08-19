5ac8ce921e37dbdf56b046f7883ad682c0d9abea
mingshun529-coder <mingshun529@gmail.com>
2026-08-19 11:42:43 +0800

    Level 1: end the chat when reveive bye and echo other input otherwise

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * A simple chatbot that greets the user and echoes commands until the user exits.
+ */
 public class Duck {
+    /**
+     * Starts the chatbot, then reads and responds to user commands.
+     *
+     * @param args command line arguments, currently unused
+     */
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         String banner = " ____             _    \n"
@@ -11,7 +21,18 @@ public class Duck {
         System.out.println("Hello! I'm Duck.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
