268003f87ec6d0dbca9aa29111a8701a2661aab1
mingshun529-coder <mingshun529@gmail.com>
2026-08-19 19:27:16 +0800

    Level-4: update Duck and link it back to Event, Deadline, Todo

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -21,8 +21,8 @@ public class Duck {
         System.out.println("Hello! I'm Duck. Quack~");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        Task[] task = new Task[100];
-        int no = -1;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -33,47 +33,63 @@ public class Duck {
                 System.out.println(line);
                 break;
             } else if (input.equals("list")) {
-
-                int track = no;
-                int id = 1;
-
                 System.out.println("Here are the tasks in your list:");
-
-                while (track >= 0) {
-
-                    System.out.println(id + "." + task[id - 1]);
-                    id++;
-                    track--;
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 System.out.println(line);
 
             } else if (input.startsWith("mark ")) {
-                int rankComplete = Integer.parseInt(input.substring(5));
-                task[rankComplete - 1].markAsDone();
+                int taskNumber = Integer.parseInt(input.substring(5));
+                tasks[taskNumber - 1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task[rankComplete -  1].toString());
+                System.out.println("  " + tasks[taskNumber - 1]);
                 System.out.println(line);
 
 
 
              } else if (input.startsWith("unmark ")) {
-                int rankComplete = Integer.parseInt(input.substring(7));
-                task[rankComplete - 1].markAsUndone();
+                int taskNumber = Integer.parseInt(input.substring(7));
+                tasks[taskNumber - 1].markAsUndone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + task[rankComplete - 1].toString());
+                System.out.println("  " + tasks[taskNumber - 1]);
                 System.out.println(line);
 
 
+
+            } else if (input.startsWith("todo ")){
+                tasks[taskCount] = new Todo(input.substring(5));
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(line);
+
+            }  else if (input.startsWith("event ")) {
+                String[] split = input.substring(6).split(" /from ");
+                tasks[taskCount] = new Event(split[0], split[1]);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(line);
+
+
+            } else if (input.startsWith("deadline ")){
+                String[] split = input.substring(9).split(" /by ");
+                tasks[taskCount] = new Deadline(split[0], split[1]);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(line);
 
             } else {
-                no++;
-                task[no] = new Task(input);
-
-
-                System.out.println("added: " + input);
-                System.out.println(line);
-
+                System.out.println(input);
             }
+
+
+
         }
     }
 }
